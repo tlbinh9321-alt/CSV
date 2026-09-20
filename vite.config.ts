f29be96b23cbf8fffda95885f2345b946cbd58d2
tlbinh9321-alt <tlbinh9321@gmail.com>
2026-09-20 23:37:53 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/CSV/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
